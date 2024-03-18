738efd5002465082b39955753991f9ff6f8960cd
veduji16 <jainved01@gmail.com>
2024-03-19 00:24:39 +0530

    Check Employee is Present or Absent

--- src/Employee_Wage/Employee_Wage.java
+++ src/Employee_Wage/Employee_Wage.java
@@ -3,5 +3,15 @@ package Employee_Wage;
 public class Employee_Wage {
     public static void main(String[] args) {
         System.out.println("...Welcome to Employee Attendance Program...\n");
+
+        final int FULL_TIME = 1;
+
+        double employeeType = Math.floor(Math.random() * 10) % 2;
+
+        if (employeeType == FULL_TIME) {
+            System.out.println("Employee is present.");
+        } else {
+            System.out.println("Employee is absent.");
+        }
     }
 }
