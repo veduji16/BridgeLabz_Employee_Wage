4414a956c9392f4099ec1ffe881c91b8b3aae828
veduji16 <jainved01@gmail.com>
2024-03-19 00:52:37 +0530

    Calculate Wages till a condition of total working hours or days is reached for a month - Assume 100 hours

--- src/Employee_Wage/Employee_Wage.java
+++ src/Employee_Wage/Employee_Wage.java
@@ -10,23 +10,37 @@ public class Employee_Wage {
         final int FULL_DAY_HOURS = 8;
         final int PART_TIME_HOURS = 4;
         final int WORKING_DAYS_PER_MONTH = 20;
+        final int MAX_WORKING_HOURS = 100;
 
         int totalWage = 0;
+        int totalWorkingHours = 0;
+        int workingDays = 0;
 
-        for (int day = 1; day <= WORKING_DAYS_PER_MONTH; day++) {
+        while (workingDays < WORKING_DAYS_PER_MONTH && totalWorkingHours < MAX_WORKING_HOURS) {
             int employeeType = (int) Math.floor(Math.random() * 10) % 3;
 
             switch (employeeType) {
                 case FULL_TIME:
                     totalWage += WAGE_PER_HOUR * FULL_DAY_HOURS;
+                    totalWorkingHours += FULL_DAY_HOURS;
+                    System.out.println("Day " + (workingDays + 1) + ": Full-time present. Daily wage: $"
+                            + (WAGE_PER_HOUR * FULL_DAY_HOURS));
                     break;
                 case PART_TIME:
                     totalWage += WAGE_PER_HOUR * PART_TIME_HOURS;
+                    totalWorkingHours += PART_TIME_HOURS;
+                    System.out.println("Day " + (workingDays + 1) + ": Part-time present. Daily wage: $"
+                            + (WAGE_PER_HOUR * PART_TIME_HOURS));
                     break;
                 default:
+                    System.out.println("Day " + (workingDays + 1) + ": Employee is absent.");
             }
+
+            workingDays++;
         }
 
-        System.out.println("Total monthly wage: $" + totalWage);
+        System.out.println("\nTotal monthly wage: $" + totalWage);
+        System.out.println("Total working hours: " + totalWorkingHours);
+        System.out.println("Working days: " + workingDays);
     }
 }
