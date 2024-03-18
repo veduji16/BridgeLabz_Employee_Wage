4e3918557269b85814e8edb62bad670c0324ac6a
veduji16 <jainved01@gmail.com>
2024-03-19 00:32:02 +0530

    Add Part time Employee & Wage

--- src/Employee_Wage/Employee_Wage.java
+++ src/Employee_Wage/Employee_Wage.java
@@ -5,14 +5,20 @@ public class Employee_Wage {
         System.out.println("...Welcome to Employee Attendance Program...\n");
 
         final int FULL_TIME = 1;
+        final int PART_TIME = 2;
         final int WAGE_PER_HOUR = 20;
         final int FULL_DAY_HOURS = 8;
+        final int PART_TIME_HOURS = 4; // Assuming part-time hours are 4
 
-        double employeeType = Math.floor(Math.random() * 10) % 2;
+        double employeeType = Math.floor(Math.random() * 10) % 3;
 
         if (employeeType == FULL_TIME) {
             int dailyWage = WAGE_PER_HOUR * FULL_DAY_HOURS;
-            System.out.println("Employee is present.");
+            System.out.println("Employee is full-time present.");
+            System.out.println("Daily wage: $" + dailyWage);
+        } else if (employeeType == PART_TIME) {
+            int dailyWage = WAGE_PER_HOUR * PART_TIME_HOURS;
+            System.out.println("Employee is part-time present.");
             System.out.println("Daily wage: $" + dailyWage);
         } else {
             System.out.println("Employee is absent.");
