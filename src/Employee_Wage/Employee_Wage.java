f0b17af81dab91f05873fe9a87d97d841f87c48a
veduji16 <jainved01@gmail.com>
2024-03-19 01:10:01 +0530

    Ability to save the Total Wage for Each Company

--- src/Employee_Wage/Employee_Wage.java
+++ src/Employee_Wage/Employee_Wage.java
@@ -1,5 +1,8 @@
 package Employee_Wage;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class Company {
     private final String companyName;
     private final int wagePerHour;
@@ -30,29 +33,37 @@ class Company {
 }
 
 public class Employee_Wage {
-    public static void main(String[] args) {
-        System.out.println("...Welcome to Employee Wage Calculation Program...\n");
 
-        Company companyA = new Company("Company A", 20, 8, 4);
-        Company companyB = new Company("Company B", 18, 7, 3);
-
-        calculateAndPrintWage(companyA);
-        System.out.println();
-        calculateAndPrintWage(companyB);
-    }
-
-    public static void calculateAndPrintWage(Company company) {
+    public static void calculateAndPrintWage(Company company, Map<String, Integer> companyWages) {
         int totalWage = 0;
         int workingDays = 20;
 
         System.out.println(company.getCompanyName());
-
         for (int day = 1; day <= workingDays; day++) {
             int employeeType = (int) Math.floor(Math.random() * 10) % 3;
             int dailyWage = company.calculateDailyWage(employeeType);
             totalWage += dailyWage;
             System.out.println("Day " + day + ": Daily wage: $" + dailyWage);
         }
+
+        companyWages.put(company.getCompanyName(), totalWage);
         System.out.println("Total monthly wage: $" + totalWage);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("...Welcome to Employee Wage Calculation Program...\n");
+
+        Map<String, Integer> companyWages = new HashMap<>();
+
+        Company companyA = new Company("Company A", 20, 8, 4);
+        Company companyB = new Company("Company B", 18, 7, 3);
+
+        calculateAndPrintWage(companyA, companyWages);
+        System.out.println();
+        calculateAndPrintWage(companyB, companyWages);
+
+        for (Map.Entry<String, Integer> entry : companyWages.entrySet()) {
+            System.out.println(entry.getKey() + " Total Monthly Wage: $" + entry.getValue());
+        }
     }
 }
