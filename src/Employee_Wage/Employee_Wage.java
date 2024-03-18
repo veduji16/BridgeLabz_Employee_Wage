8680c80d3fc46ef612d55233d9a5e9a5114bc092
veduji16 <jainved01@gmail.com>
2024-03-19 00:20:08 +0530

    Master is Created

---
+++ src/Employee_Wage/Employee_Wage.java
@@ -0,0 +1,7 @@
+package Employee_Wage;
+
+public class Employee_Wage {
+    public static void main(String[] args) {
+        System.out.println("...Welcome to Employee Attendance Program...\n");
+    }
+}
